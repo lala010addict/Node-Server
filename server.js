532855c7d7daf617324793209fb9c9caf2204308
jieyingpeng <jipeng@syr.edu>
2016-03-19 18:44:29 -0700

    server

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 
+// Add middlewares
+router.use(bodyParser.json({ limit: '10mb' }))
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(methodOverride())
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get('/', function(req, res) {
+    res.sendfile('client/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(3000, function() {
+    console.log('App listening on port 3000!');
 });
+
+
+// Create database
+var db
+
+// Expose database
+router.db = db
